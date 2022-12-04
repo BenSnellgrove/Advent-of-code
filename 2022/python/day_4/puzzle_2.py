bb057f774645aeea010c804cedb0d90add3bc452
Ben Snellgrove <bes72@aber.ac.uk>
2022-12-04 06:33:28 +0000

    2022 Day 4

---
+++ 2022/python/day_4/puzzle_2.py
@@ -0,0 +1,51 @@
+
+# check if 1 lies within 2
+def check(thingy):
+    if thingy[0][0] >= thingy[1][0] and thingy[0][0] <= thingy[1][1]:
+        return True
+    if thingy[0][1] >= thingy[1][0] and thingy[0][1] <= thingy[1][1]:
+        return True
+    return False
+
+
+inp = ""
+count = 0
+
+while True:
+
+    try:
+
+        inp = input()
+
+        if inp == "":
+            break
+
+        inp = inp.split(",")
+
+        for i in range (2):
+            inp[i] = inp[i].split("-")
+
+        for i in range(2):
+            for j in range(2):
+                inp[i][j] = int(inp[i][j])
+
+        if check(inp):
+            count += 1
+            continue
+
+
+        temp = inp[0]
+        inp[0] = inp[1]
+        inp[1] = temp
+
+        if check(inp):
+            count += 1
+            continue
+
+
+
+    except Exception:
+        pass
+
+
+print(count)
